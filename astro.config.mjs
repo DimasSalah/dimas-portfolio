340b108d102323d7a67691336782191352b1a0c0
DimasSalah <dimassalah10@gmail.com>
2026-02-14 17:50:07 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    tailwind({
+      configFile: './tailwind.config.mjs',
+      applyBaseStyles: false,
+    }),
+  ],
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        accent: "#00FF66",
+        "neutral-border": "#E2E8F0",
+        "data-label": "#64748B",
+      },
+      fontFamily: {
+        display: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
